8d6ba576398bf9ab68aa6268925b1a23ff9130de
Matthias Hannig <matthias@hannig.cc>
2018-02-04 17:45:05 +0100

    improved documentation

--- action.go
+++ action.go
@@ -10,6 +10,13 @@ type Action struct {
 	Payload interface{} `json:"payload"`
 }
 
+/*
+ Decode the payload.
+ If an action is received from MQTT, the payload
+ should be raw bytes. We decode this then on
+ demand, as the handler function should know what
+ kind of payload it requires.
+*/
 func (a Action) DecodePayload(v interface{}) error {
 	data, ok := a.Payload.([]byte)
 	if !ok {
